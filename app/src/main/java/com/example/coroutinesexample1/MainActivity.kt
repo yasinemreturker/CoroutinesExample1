e2450bf3fad10554005eb3d61ae037f8dc7174d9
byTurker <mr_turker@hotmail.com>
2021-09-06 15:39:25 +0300

    Init kotlin-android-extensions

--- app/src/main/java/com/example/coroutinesexample1/MainActivity.kt
+++ app/src/main/java/com/example/coroutinesexample1/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.coroutinesexample1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnCount.setOnClickListener {
+            tvCount.text = count++.toString()
+        }
+        btnDownloadUserData.setOnClickListener {
+            downloadUserData()
+        }
+    }
+
+    private fun downloadUserData() {
+        for (i in 1..200000) {
+            Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+        }
     }
 }
